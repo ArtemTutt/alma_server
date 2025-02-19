be29beef8d83a173412e8f6b2bfbba42467051df
Artemiy Karelov <karelov.artemiy@mail.ru>
2025-02-19 15:01:33 +0300

    firts comit

---
+++ index.js
@@ -0,0 +1,34 @@
+const { Telegraf } = require('telegraf');
+
+const bot = new Telegraf("7789595937:AAEavKAnNwqlPX4Zm0rXL04toUBgcUhqepE");
+
+// Команда /start
+bot.start((ctx) => {
+  ctx.sendPhoto(
+    { source: "./welcome.png" } 
+  ).then(() => {
+    ctx.replyWithHTML(
+      `
+      <b>Добро пожаловать в наше приложение! 🚀</b> 
+      
+      Зарегистрируйтесь и начните своё путешествие!
+      `,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Открыть Mini App",
+                web_app: { url: "https://almatest-nine.vercel.app" },
+              },
+            ],
+          ],
+        },
+      }
+    );
+  });
+});
+
+// Запуск бота
+bot.launch().then(() => console.log("🤖 Бот запущен!"));
+
